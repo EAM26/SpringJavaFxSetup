aa2389c1ced6c88b034071611b656753aed215af
EAM26 <emuller2607@gmail.com>
2025-02-23 14:34:41 +0100

    Add set functionality from database to screen.

--- src/main/java/org/eam/code/springbootjavafx/SecondViewController.java
+++ src/main/java/org/eam/code/springbootjavafx/SecondViewController.java
@@ -3,6 +3,9 @@ package org.eam.code.springbootjavafx;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import org.eam.code.springbootjavafx.Person.Person;
+import org.eam.code.springbootjavafx.Person.PersonRepository;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -10,12 +13,31 @@ import java.io.IOException;
 @Component
 public class SecondViewController {
 
+    private final PersonRepository personRepository;
     @FXML
     private Button btnSecViewBack;
+
+    public SecondViewController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
 
     @FXML
     void backToHappyView(ActionEvent event) throws IOException {
         JavaFxApp.setScreenToMyView();
+    }
+
+    @FXML
+    private Button btnSetLabel;
+
+    @FXML
+    private Label lbName;
+
+    @FXML
+    void setLabel(ActionEvent event) {
+        Person person = new Person("Piet");
+        personRepository.save(person);
+        personRepository.findById(1L).ifPresent(person1 -> lbName.setText(person1.getName()));
+
     }
 
 }
